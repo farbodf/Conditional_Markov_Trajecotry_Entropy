4a931d9dd1f41c95d98dc9991697878246187c25
Farbod Faghihi Berenjegani <faghihib@mbp-32.cs.helsinki.fi>
2016-03-18 14:20:21 +0200

    Comments are added for more readability

--- conditional_trajectory_entropy.py
+++ conditional_trajectory_entropy.py
@@ -23,7 +23,10 @@ import scipy.stats
 
 
 def local_entropy(p):
-    """Computes the local entropy at each state of the MC defined by the transition probabilities p"""
+    """
+    :param p: p is the input probability transition matrix of a Markov Chain
+    :return: H(P_k.) the local entropy of each state in the chain
+    """
     l = np.copy(p)
     l[p > 0] = np.log2(p[p > 0])
     k = np.dot(p, np.transpose(l))
@@ -32,6 +35,14 @@ def local_entropy(p):
 
 
 def get_segment_entropy(probability_transition_matrix, start, end):
+    """
+    :param probability_transition_matrix: the modified probability transition matrix that the end and destination
+    states are absorbing
+    :param start: is the starting state of the markov trajectory segment
+    :param end: is the ending state of the markov trajectory segment
+    :return: returns the entropy of the specified segment of our trajectory where the end state and the destination
+    in the probability matrix are absorbing
+    """
     p_matrix = probability_transition_matrix.copy()
     subset = np.array([x for x in range(p_matrix.shape[0]) if x != end])
     qd = p_matrix[subset]
@@ -48,12 +59,27 @@ def get_segment_entropy(probability_transition_matrix, start, end):
 
 
 def get_ad(matrix, dst):
+    """
+    :param matrix: the probability transition matrix of a markov chain that one intermediate state and destination
+     states are absorbing
+    :param dst: destination state
+    :return: the vector ad, each element 'i' in ad corresponds to the probability from state 'i' the path get trapped
+     in the destination state
+    """
     ad = np.real(scipy.linalg.eig(matrix)[1][:, destination])
     ad = ad * 1/ad[dst]
     return ad
 
 
 def absorbing_p(probability_transition_matrix, absorbing_states):
+    """
+    creates a new transition matrix in which the states contained in absorbing_states will be absorbing, in order to
+    be able to compute the probability that a trajectory will at least visit one specific intermediate state before
+    reaching the destination state
+    :param probability_transition_matrix: probability transition matrix to be changed
+    :param absorbing_states: the states that should become absorbing
+    :return: the modified matrix that contains the absorbing states
+    """
     new_p = probability_transition_matrix.copy()
     for state in absorbing_states:
         new_p[state, ] = 0
@@ -62,6 +88,12 @@ def absorbing_p(probability_transition_matrix, absorbing_states):
 
 
 def compute_p_prime(probability_transition_matrix, intermediate_state, destination):
+    """
+    :param intermediate_state: the sequence of intermediate states visited before reaching the destination
+    :param destination: destination state
+    :return: a new probability transition matrix in which the destination and the intermediate state are absorbing and
+    also some values are specifically modified for the queried segment.
+    """
     p_prime = probability_transition_matrix.copy()
     p_bar = absorbing_p(probability_transition_matrix, [intermediate_state, destination])
     ad = get_ad(p_bar, destination)
@@ -78,6 +110,15 @@ def compute_p_prime(probability_transition_matrix, intermediate_state, destinati
 
 
 def conditional_trajectory_entropy(probability_transition_matrix, source, destination, intermediate_states):
+    """
+    Here is the main algorithm as described in the "The entropy of conditional markov trajectories" paper.
+    :param probability_transition_matrix: The original probability transition matrix of our MC
+    :param source: the starting state
+    :param destination: the destination state
+    :param intermediate_states: the sequence of visited states before reaching the destination, our entropy is
+    conditioned on visiting these states
+    :return: the entropy of a conditional markov trajectories
+    """
     states = [source]
     states.extend(intermediate_states)
     sum_of_entropy_values = 0
